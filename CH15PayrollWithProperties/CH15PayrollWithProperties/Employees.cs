699eddacaf0d9a2cd53a1e89a264ef3bb0b0be1b
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-10-14 08:13:59 -0600

    Finished the classes for Employee.
    Just troubleshooting with this commit.

--- CH15PayrollWithProperties/CH15PayrollWithProperties/Employees.cs
+++ CH15PayrollWithProperties/CH15PayrollWithProperties/Employees.cs
@@ -3,19 +3,85 @@ namespace CH15PayrollWithProperties
 {
     public abstract class Employees
     {
-        string first, last;
-        int ID;
-        public Employees(string first, string last, int ID)
+        protected string first, last;
+        protected int id;
+        protected double wage;
+        protected static double totalWage;
+        public Employees(string first, string last, int idNumber, double wage)
         {
-            this.first = first;
-            this.last = last;
-            this.ID = ID;
+            First = first;
+            Last = last;
+            ID = idNumber;
+            Wage = wage;
+
         }
         public override string ToString()
         {
-            return base.ToString();//return employee number and name.
+            return $"Employee {this.ID}: {this.first} {this.last}";//return employee number and name.
         }
         public abstract decimal Earnings();
+        public string First
+        {
+            get { return first; }
+            set { first = value; }
+        }
+        public string Last
+        {
+            get { return last; }
+            set { last = value; }
+        }
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public double Wage
+        {
+            get { return this.wage; }
+            set { wage = value; }
+        }
+    }
+    public class Hourly : Employees
+    {
+        
+        int hoursWorked;
+        public Hourly(string first, string last, int idNumber, double wage, int hoursWorked)
+            : base(first, last, idNumber, wage)
+        {
+            Hours = hoursWorked;
+        }
+        
+        public int Hours
+        {
+            get { return this.hoursWorked; }
+            set { hoursWorked = value; }
+        }
+        public override decimal Earnings()
+        {
+            decimal earnings = (decimal)wage * hoursWorked;
+            return earnings;
+        }
+        public override string ToString()
+        {
+            return $"Employee {ID}: {First} {Last}, hourly wage {Wage:n2}, hours worked {Hours}";
+        }
+    }
+    public class Salary : Employees
+    {
+        public Salary(string first, string last, int idNumber, double wage, int hoursWorked)
+            : base(first, last, idNumber, wage)
+        {
+
+        }
+        public override decimal Earnings()
+        {
+            decimal earnings = (decimal)wage * 2080;
+            return earnings;
+        }
+        public override string ToString()
+        {
+            return $"Employee {ID}: {First} {Last}, Salaried, {Wage:n2} annual";
+        }
     }
 }
 
--- CH15PayrollWithProperties/CH15PayrollWithProperties/Program.cs
+++ CH15PayrollWithProperties/CH15PayrollWithProperties/Program.cs
@@ -1,9 +1,11 @@
-﻿
+﻿using CH15PayrollWithProperties;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Chapter 15 Payroll with Properties by James Sales");
+        Console.WriteLine("Chapter 15 Payroll with Properties by James Sales\n");
+
+        Employee[] array = new Employee[4];
     }
 }
