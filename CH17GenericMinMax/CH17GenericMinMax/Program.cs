689ec7e7bca973803c47c932b8a4b6e674313783
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-10-25 18:40:55 -0600

    Finished chapter 17
    I still hate unit tests.
    They're very gross.

---
+++ CH17GenericMinMax/CH17GenericMinMax/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace CH17GenericMinMax;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Chapter 17 Generic Min Max by James Sales\n");
+
+        int[] intArray = { 23, 9, 0, 210, 55, 39, 80, 31, 30 };
+        double[] doubleArray = { 29.6, 20.1, 11.2, 90.6, 11.3, 66.4 };
+        string[] stringArray = { "toaster", "oven", "fridge", "freezer", "microwave" };
+
+        //Display arrays
+        Console.Write("Array intArray contains: ");
+        DisplayArray(intArray);
+        Console.Write("Array doubleArray contains: ");
+        DisplayArray(doubleArray);
+        Console.Write("Array stringArray contains: ");
+        DisplayArray(stringArray);
+
+        Console.WriteLine($"\nMinimum for intArray is: {Minimum<int>(intArray)}");
+        Console.WriteLine($"Maximum for intArray is: {Maximum<int>(intArray)}");
+
+        Console.WriteLine($"\nMinimum for doubleArray is: {Minimum<double>(doubleArray)}");
+        Console.WriteLine($"Maximum for doubleArray is: {Maximum<double>(doubleArray)}");
+
+        Console.WriteLine($"\nMinimum for stringArray is: {Minimum<string>(stringArray)}");
+        Console.WriteLine($"Maximum for stringArray is: {Maximum<string>(stringArray)}");
+
+        //program pause statement
+        Console.WriteLine("\nPress a key to continue");
+        Console.ReadKey();
+    }
+    public static T Maximum<T>(T[] inputArray) where T : IComparable<T>
+    {
+        var max = inputArray[0];
+        foreach (T item in inputArray)
+        {
+            if (item.CompareTo(max) > 0){
+                max = item;
+            }
+        }
+        return max;
+    }
+    public static T Minimum<T>(T[] inputArray) where T : IComparable<T>
+    {
+        var min = inputArray[0];
+        foreach (T item in inputArray)
+        {
+            if (item.CompareTo(min) < 0)
+            {
+                min = item;
+            }
+        }
+        return min;
+    }
+    public static void DisplayArray(int[] inputArray)
+    {
+        foreach (var element in inputArray)
+        {
+            Console.Write($"{element} ");
+        }
+
+        Console.WriteLine();
+    }
+
+    //output double array                                 
+    public static void DisplayArray(double[] inputArray)
+    {
+        foreach (var element in inputArray)
+        {
+            Console.Write($"{element} ");
+        }
+
+        Console.WriteLine();
+    }
+
+    //output string array                                 
+    public static void DisplayArray(string[] inputArray)
+    {
+        foreach (var element in inputArray)
+        {
+            Console.Write($"{element} ");
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ CH17GenericMinMax/GenericMinMaxTests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using CH17GenericMinMax;
+namespace GenericMinMaxTests;
+
+[TestClass]
+public class UnitTest1
+{
+    //Test arrays
+    int[] intArray1 = { 1, 2, 3, 4 };
+    int[] intArray2 = { 5, 6, 7, 8 };
+    int[] intArray3 = { -1, -2, -3, -4 };
+    int[] intArray4 = { -5, -6, -7, -8 };
+    int[] intArray5 = { -1, -2, -3, 1, 2, 3 };
+
+    double[] doubleArray1 = { 1.1, 2.2, 3.3, 4.4 };
+    double[] doubleArray2 = { 5.5, 6.6, 7.7, 8.8 };
+    double[] doubleArray3 = { -1.1, -2.2, -3.3, -4.4 };
+    double[] doubleArray4 = { -5.5, -6.6, -7.7, -8.8 };
+    double[] doubleArray5 = { 1.1, 2.2, 3.3, -1.1, -2.2, -3.3 };
+
+    string[] stringArray1 = { "apple", "banana", "cantaloupe", "durian" };
+    string[] stringArray2 = { "elderberry", "fig", "grape", "huckleberry" };
+    string[] stringArray3 = { "imbe", "jackfruit", "kiwi", "lime" };
+    string[] stringArray4 = { "mango", "nectarine", "orange", "papaya" };
+    string[] stringArray5 = { "quince", "rambutan", "strawberry", "tamarind" };
+
+
+    [TestMethod]
+    public void TestMinimums()
+    {
+        Assert.AreEqual(Program.Minimum<int>(intArray1), 1);
+        Assert.AreEqual(Program.Minimum<int>(intArray2), 5);
+        Assert.AreEqual(Program.Minimum<int>(intArray3), -4);
+        Assert.AreEqual(Program.Minimum<int>(intArray4), -8);
+        Assert.AreEqual(Program.Minimum<int>(intArray5), -3);
+
+        Assert.AreEqual(Program.Minimum<double>(doubleArray1), 1.1);
+        Assert.AreEqual(Program.Minimum<double>(doubleArray2), 5.5);
+        Assert.AreEqual(Program.Minimum<double>(doubleArray3), -4.4);
+        Assert.AreEqual(Program.Minimum<double>(doubleArray4), -8.8);
+        Assert.AreEqual(Program.Minimum<double>(doubleArray5), -3.3);
+
+        Assert.AreEqual(Program.Minimum<string>(stringArray1), "apple");
+        Assert.AreEqual(Program.Minimum<string>(stringArray2), "elderberry");
+        Assert.AreEqual(Program.Minimum<string>(stringArray3), "imbe");
+        Assert.AreEqual(Program.Minimum<string>(stringArray4), "mango");
+        Assert.AreEqual(Program.Minimum<string>(stringArray5), "quince");
+    }
+    [TestMethod]
+    
+    public void TestMaximums()
+    {
+        Assert.AreEqual(Program.Maximum<int>(intArray1), 4);
+        Assert.AreEqual(Program.Maximum<int>(intArray2), 8);
+        Assert.AreEqual(Program.Maximum<int>(intArray3), -1);
+        Assert.AreEqual(Program.Maximum<int>(intArray4), -5);
+        Assert.AreEqual(Program.Maximum<int>(intArray5), 3);
+
+        Assert.AreEqual(Program.Maximum<double>(doubleArray1), 4.4);
+        Assert.AreEqual(Program.Maximum<double>(doubleArray2), 8.8);
+        Assert.AreEqual(Program.Maximum<double>(doubleArray3), -1.1);
+        Assert.AreEqual(Program.Maximum<double>(doubleArray4), -5.5);
+        Assert.AreEqual(Program.Maximum<double>(doubleArray5), 3.3);
+
+        Assert.AreEqual(Program.Maximum<string>(stringArray1), "durian");
+        Assert.AreEqual(Program.Maximum<string>(stringArray2), "huckleberry");
+        Assert.AreEqual(Program.Maximum<string>(stringArray3), "lime");
+        Assert.AreEqual(Program.Maximum<string>(stringArray4), "papaya");
+        Assert.AreEqual(Program.Maximum<string>(stringArray5), "tamarind");
+    }
+}
