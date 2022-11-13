9bfea0e081b2c223b192699d52b5bfdf9c1bcbbc
James <39015355+nojameshere@users.noreply.github.com>
2022-11-13 12:45:01 -0700

    Started CH20 but can't figure out events and delegates
    oof

---
+++ CH20AccountGrowthWithEvents/CH20AccountGrowthWithEvents/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountGrowth
+{
+    internal class Account
+    {
+        double startingBalance, interestRate, currentBalance;
+        public int numCycles;
+
+        public Account(double startingBalance, double interestRate, int numCycles)
+        {
+            StartingBalance = startingBalance;
+            InterestRate = interestRate;
+            this.numCycles = numCycles;
+            CurrentBalance = startingBalance;
+        }
+        public double StartingBalance
+        {
+            get { return startingBalance; }
+            set { startingBalance = value; }
+        }
+        public double InterestRate
+        {
+            get { return interestRate; }
+            set { interestRate = value / 100; }
+        }
+        public double CurrentBalance
+        {
+            get { return currentBalance; }
+            set { currentBalance = value; }
+        }
+        public void RunCycle()
+        {
+            double interst = currentBalance * InterestRate;
+            CurrentBalance = CurrentBalance + interst;
+        }
+    }
+}
---
+++ CH20AccountGrowthWithEvents/CH20AccountGrowthWithEvents/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+
+namespace AccountGrowth;
+public delegate void AccountDelegate(double accountBalance);
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Chapter 20 Account Growth with Events by James Sales\n");
+
+        Console.Write("What is the startin balance? $");
+        double balance = double.Parse(Console.ReadLine());
+        Console.Write("What is the interest rate? ");
+        int rate = int.Parse(Console.ReadLine());
+        Console.Write("How many cycles to grow the account? ");
+        int cycles = int.Parse(Console.ReadLine());
+        Account account = new Account(balance, rate, cycles);
+        //Events are here
+        
+        //Events are above
+        Console.WriteLine($"Account has balance of {account.CurrentBalance:C2}");
+        for (int i = 0; i < account.numCycles; i++)
+        {
+            account.RunCycle();
+            Console.WriteLine($"In cycle {i}, Account has a balance of {account.CurrentBalance:C2}");
+        }
+        Console.WriteLine($"Account has balance of {account.CurrentBalance:C2}");
+
+        Console.WriteLine("\nPress an any key to continue");
+        Console.ReadKey();
+    }
+    
+    public event AccountDelegate AccountPassHundred()
+    {
+        Console.WriteLine("** Account just passed $100 in balance");
+    }
+    public event AccountDelegate()
+    {
+        Console.WriteLine("** Account just doubled in value from initial balance");
+    }
+}
