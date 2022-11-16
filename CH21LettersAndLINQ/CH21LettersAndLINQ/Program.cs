0ba638f27973ef286ac092546cc07ad2a42c3794
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-11-16 09:30:14 -0700

    Completed first assignment of chapter21

--- CH21LettersAndLINQ/CH21LettersAndLINQ/Program.cs
+++ CH21LettersAndLINQ/CH21LettersAndLINQ/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace LettersAndLINQ;
+﻿using System.Collections;
+namespace LettersAndLINQ;
 
 public class Program
 {
@@ -6,14 +7,56 @@ public class Program
     {
         Console.WriteLine("Chapter 21 Letters and LINQ by James Sales");
         char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        char[] generatedArray = new char[26];
+        List<char> generatedArray = new List<char>();
         Random rnd = new Random();
-        for(int i = 0; i < 26; i++)
+        for(int i = 0; i < 30; i++)
         {
             int randomIndex = rnd.Next(0, 25);
-            generatedArray[i] = charArray[randomIndex];
+            generatedArray.Add(charArray[randomIndex]);
         }
-        Console.WriteLine(generatedArray);
+        Console.WriteLine("\nGenerated List:");
+        foreach(char letter in generatedArray)
+        {
+            Console.Write($"{letter} ");
+        }
+        Console.WriteLine();
+        //Order alphabetically
+        var orderByResult = from character in generatedArray
+                            orderby character
+                            select character;
+        Console.WriteLine("\nAscending Order: ");
+        foreach (char letter in orderByResult)
+        {
+            Console.Write($"{letter} ");
+        }
+        Console.WriteLine();
+        //Remove duplicates
+        var distinctAlphabetically = orderByResult.Distinct();
+        Console.WriteLine("\nAscending Order, without duplicates: ");
+        foreach (char letter in distinctAlphabetically)
+        {
+            Console.Write($"{letter} ");
+        }
+        Console.WriteLine();
+        //Order reverse alphabetically
+        var orderByDescendingResult = from character in generatedArray
+                                      orderby character descending
+                                      select character;
+        Console.WriteLine("\nDescending Order: ");
+        foreach (char letter in orderByDescendingResult)
+        {
+            Console.Write($"{letter} ");
+        }
+        Console.WriteLine();
+        //Remove duplicates for reverse alphabetically
+        var distinctReverseAlphabetically = orderByDescendingResult.Distinct();
+        Console.WriteLine("\nDescending Order, without duplicates: ");
+        foreach (char letter in distinctReverseAlphabetically)
+        {
+            Console.Write($"{letter} ");
+        }
+        Console.WriteLine();
+        Console.WriteLine($"\nNumber of unique letters: {distinctAlphabetically.Count()}");
         //Program pause statement
         Console.WriteLine("\nPress any key to continue");
         Console.ReadKey();
