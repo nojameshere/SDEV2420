9a11e16f827bee95f19716c82c275c24f33a20c1
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-11-15 17:26:55 -0700

    Closing laptop for the day.

---
+++ CH21LettersAndLINQ/CH21LettersAndLINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LettersAndLINQ;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Chapter 21 Letters and LINQ by James Sales");
+        char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        char[] generatedArray = new char[26];
+        Random rnd = new Random();
+        for(int i = 0; i < 26; i++)
+        {
+            int randomIndex = rnd.Next(0, 25);
+            generatedArray[i] = charArray[randomIndex];
+        }
+        Console.WriteLine(generatedArray);
+        //Program pause statement
+        Console.WriteLine("\nPress any key to continue");
+        Console.ReadKey();
+    }
+}
